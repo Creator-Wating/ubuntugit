c8b0e37f67305f526f8e9d93d401e665f9d19bc7
Creator-Wating <815251620@qq.com>
2021-05-20 15:50:55 +0800

    add a programmer

--- gittest.c
+++ gittest.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
+int  function(int a ,int b ,int c)
+{
+    if (a >= b){
+        if (a >= c)
+            return a;
+        else
+            return c;
+    }
+    else if  (b >= c)
+    {
+        return  b;
+    }
+    else 
+        return  c;
+}
 int main()
 {
-    printf("Hello world\n");
-    return 0;
+    int a, b, c;
+    printf("输入三个整数");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("%d",function(a,b,c));
+    return  0;
 }
 
